bbd052e2e9bed6cbf700980e6562651c38d4ce43
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-12-14 10:00:21 +0100

    drop down menus

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -7,8 +7,10 @@ import jobs from "../Icon/jobs.svg";
 import message from "../Icon/Message.svg";
 import notification from "../Icon/notification.svg";
 import work from "../Icon/work.svg";
+import { useNavigate } from "react-router-dom";
 
 const MainNavbar = () => {
+  const navigate = useNavigate();
   return (
     <Navbar expand="lg" className="navbar-main">
       <Container className="d-flex justify-content-between">
@@ -66,46 +68,56 @@ const MainNavbar = () => {
             <div className="profile-drop-down menu-size">
               <img src="https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg" alt="" className="profile-icon" />
               <NavDropdown title="Me" id="basic-nav-dropdown" className="profile-name">
-                <NavDropdown.Item href="#action/3.1" className="dropdown-main-action">
-                  Profile Name
+                <NavDropdown.Item href="#action/3.1" className="dropdown-main-action d-flex align-items-center">
+                  <span>
+                    <img src="https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg" alt="" className="profile-icon2 mr-2" />
+                  </span>
+                  <div style={{ color: "black" }}>
+                    <p className="fs-16 fw-700">User Name</p>
+                    <p className="fs-14 ">IT Specialist</p>
+                  </div>
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.1.1" className="dropdown-secondary-action">
-                  Profile Profession
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.1.2" className="dropdown-secondary-action">
-                  <Button className="btn nav-view-profile-btn">View Profile</Button>
-                </NavDropdown.Item>
+                <div href="#action/3.1.2" className="dropdown-secondary-action d-flex justify-content-center mt-2 mb-2">
+                  <Button
+                    className="btn nav-view-profile-btn fs-14"
+                    onClick={() => {
+                      navigate("/profile/1234");
+                    }}
+                  >
+                    View Profile
+                  </Button>
+                </div>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.2" className="dropdown-main-action">
+                <div href="#action/3.2" className="dropdown-main-action fs-16 fw-800 ml-4">
                   Account
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2.1" className="dropdown-secondary-action">
+                </div>
+                <NavDropdown.Item href="#action/3.2.1" className="dropdown-secondary-action fs-14">
                   Try premium for free
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2.2" className="dropdown-secondary-action">
+                <NavDropdown.Item href="#action/3.2.2" className="dropdown-secondary-action fs-14">
                   Settings & Privacy
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2.3" className="dropdown-secondary-action">
+                <NavDropdown.Item href="#action/3.2.3" className="dropdown-secondary-action fs-14">
                   Help
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2.4" className="dropdown-secondary-action">
+                <NavDropdown.Item href="#action/3.2.4" className="dropdown-secondary-action fs-14">
                   Language
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.3" className="dropdown-main-action">
+                <div href="#action/3.3" className="dropdown-main-action fs-16 fw-800 ml-4">
                   Manage
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3.1" className="dropdown-secondary-action">
+                </div>
+                <NavDropdown.Item href="#action/3.3.1" className="dropdown-secondary-action fs-14">
                   Posts & Activity
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3.2" className="dropdown-secondary-action">
+                <NavDropdown.Item href="#action/3.3.2" className="dropdown-secondary-action fs-14">
                   Job Posting Account
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Sign Out</NavDropdown.Item>
+                <NavDropdown.Item className="fs-14">Sign Out</NavDropdown.Item>
               </NavDropdown>
             </div>
-            <div className="profile-drop-down menu-size">
+            <div className="profile-drop-down menu-size work">
               <img src={work} alt="" className="nav-menu-icon" />
               <NavDropdown title="Work" id="basic-nav-dropdown" className="profile-name ">
                 <NavDropdown.Item href="#action/3.1" className="dropdown-main-action">

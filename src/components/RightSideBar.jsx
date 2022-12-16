9360ba6c714ce82cfc322d238ec0d90d5cc60240
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-12-16 14:38:11 +0100

    responsivnes on profile

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -40,7 +40,7 @@ const RightSideBar = () => {
   const allProfiles = useSelector((state) => state.profiles.allProfiles);
 
   return (
-    <Col className="d-flex flex-column align-items-end " sm={12} md={12} lg={3}>
+    <Col className="d-flex flex-column align-items-end ">
       <ul className="sidebarUL mb-1">
         <EditLi text="Edit public profile & URL" />
         <EditLi text="Add profile in another language" />
